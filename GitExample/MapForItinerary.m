e972249f36f900d2a80183c69246c71b3b31e42c
mcguirephil01 <mcguirephil01@gmail.com>
2014-07-10 15:53:27 -0400

    Another Message

--- GitExample/MapForItinerary.m
+++ GitExample/MapForItinerary.m
@@ -9,5 +9,10 @@
 #import "MapForItinerary.h"
 
 @implementation MapForItinerary
+-(void)fakeMethod
+{
+    // This is a fake method just to make the discard action clear for you
+    NSlog(@"Discarding chcanges allow you to get the latest revision you have worked on it.");
+}
 
 @end
