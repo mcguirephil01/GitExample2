73e1ea6206d18d8db76690cda622dcf2516c2a61
mcguirephil01 <mcguirephil01@gmail.com>
2014-07-10 15:24:52 -0400

    Initial Commit

---
+++ GitExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitExample
+//
+//  Created by McGuire, Philip on 7/10/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitExample
+//
+//  Created by McGuire, Philip on 7/10/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
