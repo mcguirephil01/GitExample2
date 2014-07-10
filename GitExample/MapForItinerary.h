a1829680ea58ae7ce560b142d7ac32c3e32bd5d9
mcguirephil01 <mcguirephil01@gmail.com>
2014-07-10 15:46:47 -0400

    Committing to Branch

---
+++ GitExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitExample
+//
+//  Created by McGuire, Philip on 7/10/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitExample
+//
+//  Created by McGuire, Philip on 7/10/14.
+//  Copyright (c) 2014 McGuire, Philip. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
